2a52c87ed8b8ae7e9fe456147d59564275e44c87
amaged1896 <ahmed.maged1896@gmail.com>
2024-03-03 01:21:44 +0200

    car model

--- DB/models/car.model.js
+++ DB/models/car.model.js
@@ -1,6 +1,40 @@
 import mongoose from "mongoose";
 
-const carSchema = new mongoose.Schema({});
+const carSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Car name is required'],
+        trim: true,
+    },
+    category: {
+        type: String,
+        required: [true, 'Car category is required'],
+        trim: true,
+    },
+    tankCapacity: {
+        type: Number,
+        required: [true, 'Car tank capacity is required']
+    },
+    average: {
+        type: Number,
+        required: [true, 'Car average KM is required']
+    },
+    transmission: {
+        enum: ['auto', 'manual'],
+    },
+    capacity: {
+        type: Number,
+        required: [true, 'Car capacity is required']
+    },
+    isRented: {
+        type: Boolean,
+        default: false
+    },
+    priceForDay: {
+        type: Number,
+        required: [true, 'Car price for day is required']
+    },
+});
 
 const Car = mongoose.model("Car", carSchema);
 
