4cf57ac73636edd9a29479dfac69a89241df08d4
amaged1896 <ahmed.maged1896@gmail.com>
2024-03-04 01:50:59 +0200

    Add wishlist functionality to user controller

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -1,10 +1,47 @@
 import User from "../../../DB/models/user.model.js";
+import catchAsync from "../../utils/catchAsync.js";
 import {
   getAll,
   getOne,
   deleteOne,
   updateOne,
 } from "../controllers.factory.js";
+
+export const addToWishlist = catchAsync(async (req, res, next) => {
+  const user = await User.findByIdAndUpdate(req.user.id,
+    { $addToSet: { wishlist: req.body.carId } },
+    { new: true }
+  );
+
+  if (!user) {
+    return next(new AppError(`user id is not exists`, 404));
+  }
+  sendData(200, "success", "Car added to wishlist successfully", null, res);
+});
+
+export const clearWishlist = catchAsync(async (req, res, next) => {
+  const user = await User.findByIdAndUpdate(req.user.id,
+    { $set: { wishlist: [] } },
+    { new: true }
+  );
+
+  if (!user) {
+    return next(new AppError(`user id is not exists`, 404));
+  }
+  sendData(200, "success", "Wishlist cleared successfully", null, res);
+});
+
+export const removeFromWishlist = catchAsync(async (req, res, next) => {
+  const user = await User.findByIdAndUpdate(req.user.id,
+    { $pull: { wishlist: req.body.carId } },
+    { new: true }
+  );
+
+  if (!user) {
+    return next(new AppError(`user id is not exists`, 404));
+  }
+  sendData(200, "success", "Car removed from wishlist successfully", null, res);
+});
 
 const populateObj = [
   {
