2031201818c9235fa722d46d8847ccfc8e5da406
Mohamed Ahmed Gaber <154239845+Mohamed-G-AlNagar@users.noreply.github.com>
2024-03-03 12:33:48 +0200

    updated

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,25 +1,31 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
+import { emailTemplate } from "./emailTemplate.js";
 
-export const sendEmail = async ({ email, subject, html, attachments }) => {
-    // 1) Create a transporter
-    const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASSWORD,
-        }
-    });
+export const sendEmail = async ({ email, subject, url, message, html }) => {
+  // 1) Create a transporter
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
 
-    // 2) Define the email options
-    const mailOptions = {
-        from: `ITI - Ecommerce < ${process.env.EMAIL_USERNAME} >`,
-        to: email,
-        subject,
-        html,
-        attachments,
-    };
+  // 2) Define the email options
+  const mailOptions = {
+    from: `ITI-Final Project < ${process.env.EMAIL_USERNAME} >`,
+    to: email,
+    subject,
+    // html,
+    html: emailTemplate(url, message),
+  };
 
-    // 3) send the email
-    const sendEmail = await transporter.sendMail(mailOptions);
-    return sendEmail.accepted.length < 1 ? false : true;
+  // 3) send the email
+  const sendEmail = await transporter.sendMail(mailOptions);
+  return sendEmail.accepted.length < 1 ? false : true;
 };
+
+export default sendEmail;
