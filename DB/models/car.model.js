d9ca7f6a2aaa8e674b2ad35098069d1107707a88
Mohamed Ahmed Gaber <154239845+Mohamed-G-AlNagar@users.noreply.github.com>
2024-03-03 18:56:01 +0200

    add Car Owner

--- DB/models/car.model.js
+++ DB/models/car.model.js
@@ -1,48 +1,61 @@
 import mongoose from "mongoose";
 
-const carSchema = new mongoose.Schema({
+const carSchema = new mongoose.Schema(
+  {
+    ownerId: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Car owner is required"],
+    },
     name: {
-        type: String,
-        required: [true, 'Car name is required'],
-        trim: true,
+      type: String,
+      required: [true, "Car name is required"],
+      trim: true,
     },
     category: {
-        type: String,
-        required: [true, 'Car category is required'],
-        trim: true,
+      type: String,
+      required: [true, "Car category is required"],
+      trim: true,
     },
     tankCapacity: {
-        type: Number,
-        required: [true, 'Car tank capacity is required']
+      type: Number,
+      required: [true, "Car tank capacity is required"],
     },
     average: {
-        type: Number,
-        required: [true, 'Car average KM is required']
+      type: Number,
+      required: [true, "Car average KM is required"],
     },
     transmission: {
-        enum: ['auto', 'manual'],
+      enum: ["auto", "manual"],
     },
     capacity: {
-        type: Number,
-        required: [true, 'Car capacity is required']
+      type: Number,
+      required: [true, "Car capacity is required"],
     },
     isRented: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     priceForDay: {
-        type: Number,
-        required: [true, 'Car price for day is required']
+      type: Number,
+      required: [true, "Car price for day is required"],
     },
     location: {
-        type: String,
-        required: [true, 'Please Enter car location']
+      type: String,
+      required: [true, "Please Enter car location"],
     },
     totalKM: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0,
     },
-}, { timestamps: true, virtuals: true, toJSON: { virtuals: true }, toObject: { virtuals: true } });
+  },
+  {
+    timestamps: true,
+    virtuals: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
 const Car = mongoose.model("Car", carSchema);
 
