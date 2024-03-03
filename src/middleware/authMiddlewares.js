5e594ff8a0e3b0e2194a4447567a6d020d306446
Mohamed Ahmed Gaber <154239845+Mohamed-G-AlNagar@users.noreply.github.com>
2024-03-03 12:36:18 +0200

    modify the auth middlewares

--- src/middleware/authMiddlewares.js
+++ src/middleware/authMiddlewares.js
@@ -1,13 +1,13 @@
 import { promisify } from "util";
 import jwt from "jsonwebtoken";
 
-import User from "../../DB/models/user/user.model.js";
+import User from "../../DB/models/user.model.js";
 import catchAsync from "../utils/catchAsync.js";
 import AppError from "../utils/appError.js";
 //----------------------------------------------------
 
 //? add my object to the req.params as middleware to reuse
-export const addMeToURL = (req, res, next) => {
+export const adduserIdToURL = (req, res, next) => {
   req.params.id = req.user.id;
   next();
 };
@@ -16,10 +16,11 @@ export const addMeToURL = (req, res, next) => {
 export const accessRestrictedTo = (...allowedRoles) => {
   return (req, res, next) => {
     if (!allowedRoles.includes(req.user.role)) {
-      return AppError(
-        res,
-        "your account is not allowed to perform this action, call the admin for permission",
-        403
+      return next(
+        new AppError(
+          "your account is not allowed to perform this action, call the admin for permission",
+          403
+        )
       );
     }
     next();
@@ -38,10 +39,11 @@ export const protect = catchAsync(async (req, res, next) => {
   }
 
   if (!token) {
-    return AppError(
-      res,
-      "You Are Not Logged in, please login first and try again",
-      401
+    return next(
+      new AppError(
+        "You Are Not Logged in, please login first and try again",
+        401
+      )
     );
   }
 
@@ -53,19 +55,21 @@ export const protect = catchAsync(async (req, res, next) => {
   //? 3- check if user still exists
   const loggingInUser = await User.findById(decodeToken.id);
   if (!loggingInUser) {
-    return AppError(
-      res,
-      "This token user is no longer exist, please try another login",
-      401
+    return next(
+      new AppError(
+        "This token user is no longer exist, please try another login",
+        401
+      )
     );
   }
 
   //? 4- check if user changed password after token was issued
   if (loggingInUser.IsPasswordChangedAfter(decodeToken.iat)) {
-    return AppError(
-      res,
-      "this user password was changed after token was issued, please login again.",
-      401
+    return next(
+      new AppError(
+        "this user password was changed after token was issued, please login again.",
+        401
+      )
     );
   }
 
@@ -78,28 +82,32 @@ export const isVerified = catchAsync(async (req, res, next) => {
   if (req.user.isVerified) {
     next();
   } else {
-    return AppError(
-      res,
-      "Your account is not verified, please verify your account first",
-      403
+    return next(
+      new AppError(
+        "Your account is not verified, please verify your account first",
+        403
+      )
     );
   }
 });
 
 //-----------------------------------------------------
 //? middleware to only allow the creator or admin to perform the next action
-export const isCreatorUserOrAdmin = (Model, modelName) => {
+export const isCreaterUserOrAdmin = (Model, modelName) => {
   return catchAsync(async (req, res, next) => {
     const document = await Model.findById(req.params.id);
-    if (!document) return AppError(res, `This ${modelName} is not found`, 401);
+    if (!document)
+      return next(new AppError(res, `This ${modelName} is not found`, 401));
     if (
       req.user.role !== "admin" &&
       String(document.createdBy) !== String(req.user._id)
     ) {
-      return AppError(
-        res,
-        `Only Admin or creatorUser can perform this to ${modelName}`,
-        401
+      return next(
+        new AppError(
+          res,
+          `Only Admin or createrUser can perform this to ${modelName}`,
+          401
+        )
       );
     }
     next();
