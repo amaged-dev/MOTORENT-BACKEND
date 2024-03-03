a22310184528cde9b20a3b2fae870b6111be7b49
amaged1896 <ahmed.maged1896@gmail.com>
2024-03-03 23:10:26 +0200

    create brand model

---
+++ DB/models/brand.model.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const brandSchema = new mongoose.Schema({
+    brand: {
+        type: String,
+        required: [true, "Brand name is required"],
+        trim: true,
+        uppercase: true,
+    },
+});
+const Brand = mongoose.model("Brand", brandSchema);
+export default Brand;
