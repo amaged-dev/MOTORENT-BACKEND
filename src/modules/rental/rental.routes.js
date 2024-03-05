9d334aa53f5eacfbf159062f9f4872f9c0467df2
amaged1896 <ahmed.maged1896@gmail.com>
2024-03-06 01:41:44 +0200

    Add rental routes and controllers

---
+++ src/modules/rental/rental.routes.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import * as rentalControllers from './rental.controller.js';
+import { accessRestrictedTo, isCreatorUserOrAdmin, protect } from './../../middleware/authMiddlewares';
+import Rental from '../../../DB/models/rental.model.js';
+const rentalRouter = express.Router();
+
+rentalRouter.use(protect);
+
+rentalRouter.get('/getRecentRentals', rentalControllers.getRecentRentals);
+rentalRouter.get('/getRentalsByUserId', rentalControllers.getRentalsByUserId);
+rentalRouter.get('/getRentalsByCarId', rentalControllers.getRentalsByCarId);
+rentalRouter.get('/getRentalsByDate', rentalControllers.getRentalsByDate);
+rentalRouter.get('/getOneRental', rentalControllers.getOneRental);
+rentalRouter.delete('/deleteOneRental', rentalControllers.deleteOneRental);
+rentalRouter.post('/createRental', rentalControllers.createRental);
+
+rentalRouter.get('/getMyRentals', isCreatorUserOrAdmin(Rental, 'Rental'), rentalControllers.getMyRentals);
+rentalRouter.get('/getAllRentals', accessRestrictedTo('admin'), rentalControllers.getAllRentals);
+
+export default rentalRouter;
