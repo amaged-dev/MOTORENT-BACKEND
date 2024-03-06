820bb3cf5e7a00294ea7859f35388d72a8b4b2e0
amaged1896 <ahmed.maged1896@gmail.com>
2024-03-06 02:50:52 +0200

    Add brand and review controllers

---
+++ src/modules/brand/brand.controller.js
@@ -0,0 +1,20 @@
+import AppError from "../../utils/appError.js";
+import catchAsync from "../../utils/catchAsync.js";
+import { sendData } from "../../utils/sendData.js";
+import { createOne, deleteOne, updateOne } from "../controllers.factory.js";
+import Brand from './../../../DB/models/brand.model.js';
+
+
+export const getBrand = catchAsync(async (req, res, next) => {
+    const brand = await Brand.findOne({ $or: { _id: req.params.id, brand: req.body.brand } });
+    if (!brand) {
+        return next(new AppError("This Brand Id Not Exists", 404));
+    }
+
+    sendData(200, "success", "Requested data successfully fetched", brand, res);
+});
+
+export const addBrand = createOne(Brand);
+export const deleteBrand = deleteOne(Brand);
+export const updateBrand = updateOne(Brand);
+export const getAllBrands = getAll(Brand);
--- src/modules/review/review.controller.js
+++ src/modules/review/review.controller.js
@@ -1,11 +1,24 @@
 import Review from "../../../DB/models/review.model.js";
-import {
-  getAll,
-  getOne,
-  deleteOne,
-  updateOne,
-  createOne,
-} from "../controllers.factory.js";
+import catchAsync from "../../utils/catchAsync.js";
+import { deleteOne, getAll, getOne, updateOne } from "../controllers.factory.js";
+import Car from './../../../DB/models/car.model.js';
+import Rental from './../../../DB/models/rental.model.js';
+
+export const addReview = catchAsync(async (req, res, next) => {
+  // check car existence
+  const carExist = await Car.findById(req.body.carId);
+  if (!carExist) {
+    return next(new AppError("Car not found", 404));
+  }
+  // check if the user has rented the car
+  const userRentedCar = await Rental.findOne({ renterId: req.user.id, car: req.body.carId });
+  if (!userRentedCar) {
+    return next(new AppError("You have not rented this car", 404));
+  }
+  const newReview = await Review.create(req.body);
+  sendData(201, "success", "Review added successfully", newReview, res);
+});
+
 
 const populateObj = [
   {
@@ -24,6 +37,4 @@ export const getReview = getOne(Review, populateObj);
 
 export const deleteReview = deleteOne(Review);
 
-export const updateCar = updateOne(Review);
-
-export const addReview = createOne(Review);
+export const updateReview = updateOne(Review);
