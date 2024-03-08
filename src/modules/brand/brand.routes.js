7eb443780f127004c542f573813c8198d576cb70
Mohamed Ahmed Gaber <154239845+Mohamed-G-AlNagar@users.noreply.github.com>
2024-03-08 10:46:30 +0200

    adjust the brand validation messages

--- src/modules/brand/brand.routes.js
+++ src/modules/brand/brand.routes.js
@@ -2,15 +2,15 @@ import express from 'express';
 import * as brandControllers from './brand.controller.js';
 import { accessRestrictedTo } from '../../middleware/authMiddlewares.js';
 import { isValid } from '../../middleware/validation.js';
-import { idSchema, brandSchema } from './brand.validation.js';
+import { idSchema, updateBrandSchema, addBrandSchema } from './brand.validation.js';
 const brandRouter = express.Router();
 
 brandRouter.get('/', brandControllers.getAllBrands);
-brandRouter.post('/', accessRestrictedTo('admin'), brandControllers.addBrand);
+brandRouter.post('/', accessRestrictedTo('admin'), isValid(addBrandSchema), brandControllers.addBrand);
 
 brandRouter.route('/:id')
     .get(isValid(idSchema), brandControllers.getBrand)
-    .patch(isValid(brandSchema), accessRestrictedTo('admin'), brandControllers.updateBrand)
+    .patch(isValid(updateBrandSchema), accessRestrictedTo('admin'), brandControllers.updateBrand)
     .delete(isValid(idSchema), accessRestrictedTo('admin'), brandControllers.deleteBrand);
 
 export default brandRouter;
--- src/modules/brand/brand.validation.js
+++ src/modules/brand/brand.validation.js
@@ -2,10 +2,37 @@ import Joi from "joi";
 import { isValidObjectId } from './../../middleware/validation.js';
 
 export const idSchema = Joi.object({
-    id: Joi.string().custom(isValidObjectId).required(),
+    id: Joi.string()
+        .custom(isValidObjectId, 'custom validation')
+        .required()
+        .messages({
+            'any.required': 'id is required',
+            'string.base': 'id must be a string',
+            'custom validation': 'id is not a valid ObjectId'
+        }),
 });
+export const updateBrandSchema = Joi.object({
+    id: Joi.string()
+        .custom(isValidObjectId, 'custom validation')
+        .required()
+        .messages({
+            'any.required': 'id is required',
+            'string.base': 'id must be a string',
+            'custom validation': 'id is not a valid ObjectId'
+        }),
+    brand: Joi.string()
+        .required()
+        .messages({
+            'any.required': 'Brand is required',
+            'string.base': 'Brand must be a string'
+        })
+});
+export const addBrandSchema = Joi.object({
 
-export const brandSchema = Joi.object({
-    id: Joi.string().custom(isValidObjectId).required(),
-    brand: Joi.string().required()
+    brand: Joi.string()
+        .required()
+        .messages({
+            'any.required': 'Brand is required',
+            'string.base': 'Brand must be a string'
+        })
 });
